b651960230a0b207a3f1cfb97fb66b106f2272db
Korimilli Jogeswara Ganga Durgarao <155608076+jogeswara123@users.noreply.github.com>
2026-06-08 10:14:36 +0530

    Time: 324 ms (5.10%) | Memory: 119.8 MB (99.82%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        List<Integer> c=new ArrayList<>();
+        List<Integer> b=new ArrayList<>();
+        List<Integer> n=new ArrayList<>();
+        for(int i:nums){
+            if(i<pivot){
+                c.add(i);
+            }
+            else if(i==pivot){
+                n.add(i);
+            }
+            else{
+                b.add(i);
+            }
+        }
+        int m[]=new int[c.size()+b.size()+n.size()];
+        int j=0;
+        for(int i:c){
+            System.out.print(i);
+            m[j++]=i;
+        }
+        for(int i:n){
+            m[j++]=i;
+        }
+        for(int i:b){
+            m[j++]=i;
+        }
+        return m;
+    }
+}
