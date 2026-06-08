08b45a45faa38af2ba3e2ef2ccfe85c7d2e9f076
Korimilli Jogeswara Ganga Durgarao <155608076+jogeswara123@users.noreply.github.com>
2026-06-08 11:53:57 +0530

    Time: 2 ms (98.16%) | Memory: 46.9 MB (55.73%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        List<Integer> c=new ArrayList<>();
+        for(int i:asteroids){
+            if(i>0){
+                c.add(i);
+            }
+            else{
+                if(c.size()==0){
+                    c.add(i);
+                }
+                else{
+                for(int j=c.size()-1;j>=0;j--){
+                    if(c.get(j)<0){
+                        c.add(i);
+                        break;
+                    }
+                    else{
+                    if(c.get(j)+i<0&&c.get(j)>0){
+                        c.remove(j);
+                    }
+                    else if(c.get(j)+i==0){
+                        c.remove(j);
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                    if(c.size()==0){
+                        c.add(i);
+                    }
+                    }
+                }
+                }
+            }
+        }
+        int m[]=new int[c.size()];
+        int j=0;
+        for(int i:c){
+            m[j++]=i;
+        }
+        return m;
+    }
+}
