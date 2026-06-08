1e601884c2030a4db53390a4d1f9402c6b1045a0
Korimilli Jogeswara Ganga Durgarao <155608076+jogeswara123@users.noreply.github.com>
2026-06-08 14:11:22 +0530

    Time: 26 ms (76.61%) | Memory: 107.5 MB (50.92%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long m=mass;
+        for(int i:asteroids){
+            if(m>=i){
+                m+=i;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
