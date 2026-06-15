7cafb57610543705c5435dae31ff345a2129ff52
Korimilli Jogeswara Ganga Durgarao <155608076+jogeswara123@users.noreply.github.com>
2026-06-15 10:36:40 +0530

    Time: 1057 ms (59.26%) | Memory: 42.2 MB (25.50%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int n=num/2;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+    }
+}
