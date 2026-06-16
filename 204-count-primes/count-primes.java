80f4cc2820484ed1c2315532e7ef07aed8fc7ae0
K Jogeswara Ganga Durgarao <155608076+jogeswara123@users.noreply.github.com>
2026-06-16 09:54:40 +0530

    Time: 124 ms (12.21%) | Memory: 68.6 MB (12.17%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+       boolean a[]=new boolean[n];
+       if(n<=2){
+        return 0;
+       }
+       Arrays.fill(a,true);
+       a[0]=false;
+       a[1]=false;
+       for(int i=2;i*i<n;i++){
+          if(a[i]){
+            for(int j=i*i;j<n;j+=i){
+                a[j]=false;
+            }
+          }
+       }
+       List<Integer> b=new ArrayList<>();
+       for(int i=2;i<n;i++){
+         if(a[i]){
+            b.add(i);
+         }
+       }
+       return b.size();
+    }
+}
