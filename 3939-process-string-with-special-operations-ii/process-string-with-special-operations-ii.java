6e5e8888a3f08fa703b4ab0d31af9e9c302a395a
K Jogeswara Ganga Durgarao <155608076+jogeswara123@users.noreply.github.com>
2026-06-17 10:04:35 +0530

    Time: 12 ms (100.00%) | Memory: 53.8 MB (7.69%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -1,47 +1,11 @@
+/*
+ * @lc app=leetcode id=3614 lang=java
+ *
+ * [3614] Process String with Special Operations II
+ */
+
 class Solution {
     public char processStr(String s, long k) {
-       int n = s.length();
-
-        long[] len = new long[n + 1];
-
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {
-                len[i + 1] = len[i] + 1;
-            } else if (ch == '*') {
-                len[i + 1] = Math.max(0, len[i] - 1);
-            } else if (ch == '#') {
-                len[i + 1] = Math.min((long)1e15, len[i] * 2);
-            } else { // %
-                len[i + 1] = len[i];
-            }
-        }
-
-        if (k >= len[n]) {
-            return '.';
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {
-                if (k == len[i]) {
-                    return ch;
-                }
-            } else if (ch == '#') {
-                k %= len[i];
-            } else if (ch == '%') {
-                k = len[i] - 1 - k;
-            } else { // *
-                if (len[i] > len[i + 1]) {
-                    if (k == len[i + 1]) {
-                        return '.';
-                    }
-                }
-            }
-        }
-
-        return '.';
+        return CompletableFuture.supplyAsync(() -> Optional.of(new long[]{0, k, '.', 0}).map(st -> (char) (s.chars().mapToLong(c -> (st[0] = Character.isLowerCase(c) ? st[0] + 1 : (c == '*' && st[0] > 0 ? st[0] - 1 : (c == '#' ? st[0] * 2 : st[0]))) * 0L).sum() * 0L + (st[1] >= st[0] ? 0L : IntStream.range(0, s.length()).map(inv -> s.length() - 1 - inv).mapToLong(i -> st[3] == 1 ? 0L : (Character.isLowerCase(s.charAt(i)) ? (st[1] == st[0] - 1 ? (st[2] = s.charAt(i)) * 0L + (st[3] = 1) * 0L : (st[0]--) * 0L) : (s.charAt(i) == '*' ? (st[0]++) * 0L : (s.charAt(i) == '#' ? (st[0] /= 2) * 0L + (st[1] >= st[0] ? (st[1] -= st[0]) : 0L) * 0L : (s.charAt(i) == '%' ? (st[1] = st[0] - 1 - st[1]) * 0L : 0L))))).sum() * 0L) + st[2])).get()).join();
     }
 }
